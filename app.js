562f408898f9dae693d751609c1e9a5b60152f39
jiwomdf <mdf.jiwo@gmail.com>
2020-07-10 19:49:56 +0700

    use mvc pattern

--- app.js
+++ app.js
@@ -1,69 +1,28 @@
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-const Blog = require('./model/blog')
+const blogRoutes = require('./routes/blogRoutes')
 
 const app = express()
 
 // connect to mongoDB
-const dbURI = 'mongodb+srv://expressApi:MantapDjiwa123@nodetuts.akanr.mongodb.net/note-tuts?retryWrites=true&w=majority'
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(res => app.listen(3000))
     .catch(err => console.log(err))
 
 //middleware & static files
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-/*
-//mongoose and mongo sandbox routes
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'new blog 2',
-        snippet: 'about my new blog 2',
-        body: 'more about my new blog 2'
-    })
-
-    blog.save()
-        .then(retval => res.send(retval))
-        .catch(err => console.log(err))
-})
-
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-        .then(retval => res.send(retval))
-        .catch(err => console.log(err))
-})
-
-app.get('/single-blog', (req, res) => {
-    Blog.findById('5f0833c3cfce761fe4bf592a')
-        .then(retval => res.send(retval))
-        .catch(err => console.log(err))
-})
-*/
+//blog routes
+app.use('/blogs/', blogRoutes)
 
 //register view engine
 app.set('view engine', 'ejs') // --> default looking in `views` directory
 
 app.get('/', (req, res) => {
     res.redirect('/blogs')
-})
-
-//blog routes
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then(retval => {
-            res.render('index', { title: 'All Blog', blogs: retval })
-        })
-        .catch(err => console.log(err))
-})
-
-app.get('/about', (req, res) => {
-    res.render('about', { title: 'About' })
-})
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' })
 })
 
 
---
+++ controllers/blogController.js
@@ -0,0 +1,44 @@
+const Blog = require('../model/blog')
+
+const blog_index = (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then(retval => res.render('index', { title: 'All Blog', blogs: retval }))
+        .catch(err => console.log(err))
+}
+
+const blog_details = (req, res) => {
+    const id = req.params.id
+
+    Blog.findById(id)
+        .then(retVal => res.render('detail', { blog: retVal, title: 'Blog Details' }))
+        .catch(err => console.log(err))
+}
+
+const blog_create_post = (req, res) => {
+
+    const blog = new Blog(req.body)
+
+    blog.save()
+        .then(retval => res.redirect('/blogs'))
+        .catch(err => console.log(err))
+}
+
+const blog_create_get = (req, res) => {
+    res.render('create', { title: 'Create a new blog' })
+}
+
+const blog_delete = (req, res) => {
+    const id = req.params.id
+
+    Blog.findByIdAndDelete(id)
+        .then(retVal => res.json({ redirect: '/blogs' }))
+        .catch(err => console.log(err))
+}
+
+module.exports = {
+    blog_index,
+    blog_details,
+    blog_create_post,
+    blog_create_get,
+    blog_delete
+}
