d2981354dd12b68694fa6819554caee676a1debc
jiwomdf <mdf.jiwo@gmail.com>
2020-07-10 16:47:43 +0700

    remove node_module and update the app.js + blog.js

--- app.js
+++ app.js
@@ -1,28 +1,61 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Blog = require('./model/blog')
 
 const app = express()
 
 // connect to mongoDB
 const dbURI = 'mongodb+srv://expressApi:MantapDjiwa123@nodetuts.akanr.mongodb.net/note-tuts?retryWrites=true&w=majority'
-
-
-app.listen(3000)
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(res => app.listen(3000))
+    .catch(err => console.log(err))
 
 //middleware & static files
 app.use(express.static('public'))
 app.use(morgan('dev'))
 
+/*
+//mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog 2',
+        snippet: 'about my new blog 2',
+        body: 'more about my new blog 2'
+    })
+
+    blog.save()
+        .then(retval => res.send(retval))
+        .catch(err => console.log(err))
+})
+
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+        .then(retval => res.send(retval))
+        .catch(err => console.log(err))
+})
+
+app.get('/single-blog', (req, res) => {
+    Blog.findById('5f0833c3cfce761fe4bf592a')
+        .then(retval => res.send(retval))
+        .catch(err => console.log(err))
+})
+*/
+
 //register view engine
 app.set('view engine', 'ejs') // --> default looking in `views` directory
 
 app.get('/', (req, res) => {
-    const blogs = [
-        { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-        { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-        { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-    ]
-    res.render('index', { title: 'Home', blogs })
+    res.redirect('/blogs')
+})
+
+//blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then(retval => {
+            res.render('index', { title: 'All Blog', blogs: retval })
+        })
+        .catch(err => console.log(err))
 })
 
 app.get('/about', (req, res) => {
